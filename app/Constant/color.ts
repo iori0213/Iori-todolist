03b8279a267c0d59d0ae292c4df21f3436d95d85
Bryan870213yoyo <43610372+Bryan870213yoyo@users.noreply.github.com>
2022-02-06 14:32:10 +0800

    add Card component and colors constant

---
+++ app/Constant/color.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    primaryColor: "#69BC5C",
+    underline: "#2CB843",
+    accent: "#56A04A",
+    day: "#CAFFD6",
+    brightText: "#FFF",
+};
---
+++ app/components/Card.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+    ActivityIndicator,
+    StyleProp,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    ViewStyle,
+} from "react-native";
+import { colors } from "../Constant/color";
+
+interface Props {
+    style?: StyleProp<ViewStyle>;
+    onPress?: (any: any) => void;
+    onLongPress?: () => void;
+    delayLongPress?: number;
+    loading?: boolean;
+}
+const Card: React.FC<Props> = ({
+    children,
+    style,
+    onPress,
+    onLongPress,
+    delayLongPress,
+    loading,
+}) => {
+    return (
+        <View>
+            <TouchableOpacity
+                onPress={onPress}
+                onLongPress={onLongPress}
+                delayLongPress={delayLongPress || 100}
+            >
+                <View style={[styles.card, style]}>
+                    {loading !== undefined ? (
+                        loading === true ? (
+                            <ActivityIndicator color={colors.primaryColor} />
+                        ) : (
+                            children
+                        )
+                    ) : (
+                        children
+                    )}
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        padding: 20,
+        alignItems: "center",
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: "white",
+        borderRadius: 10,
+    },
+});
+
+export default Card;
